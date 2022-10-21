249fda078ec0f5c4f95d653f46de84b567f561b1
Javlon Fattoev <javlonfattoev@gmail.com>
2022-10-21 12:09:21 +0500

    service container extension

---
+++ JFA.DependencyInjection/DependencyResolverExtensions.cs
@@ -0,0 +1,22 @@
+﻿using JFA.DependencyContainer;
+
+namespace JFA.DependencyInjection;
+
+public static class DependencyResolverExtensions
+{
+    public static void AddServicesFromAttribute(this DependencyResolver resolver)
+    {
+        foreach (var service in AttributeServices.GetServices())
+        {
+            switch (service.Lifetime)
+            {
+                case ELifetime.Transient:
+                    service.BaseTypes.ForEach(baseType => resolver.Services.AddTransient(baseType, service.Type)); break;
+                case ELifetime.Scoped:
+                    service.BaseTypes.ForEach(baseType => resolver.Services.AddScoped(baseType, service.Type)); break;
+                case ELifetime.Singleton:
+                    service.BaseTypes.ForEach(baseType => resolver.Services.AddSingleton(baseType, service.Type)); break;
+            }
+        }
+    }
+}
--- JFA.DependencyInjection/ServiceCollectionExtensions.cs
+++ JFA.DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System.Reflection;
 
 namespace JFA.DependencyInjection;
 
@@ -7,7 +6,7 @@ public static class ServiceCollectionExtensions
 {
     public static void AddServicesFromAttribute(this IServiceCollection services)
     {
-        foreach (var service in GetServices())
+        foreach (var service in AttributeServices.GetServices())
         {
             switch (service.Lifetime)
             {
@@ -19,44 +18,5 @@ public static class ServiceCollectionExtensions
                     service.BaseTypes.ForEach(baseType => services.AddSingleton(baseType, service.Type)); break;
             }
         }
-    }
-
-    public static List<ServiceInfo> GetServices(this AppDomain domain)
-    {
-        return domain.GetAssemblies()
-            .SelectMany(s => s.GetTypes())
-            .Where(Filter)
-            .Select(GetService).ToList();
-    }
-
-    private static bool Filter(Type type) =>
-        type.IsClass && 
-        !type.IsAbstract &&
-        type.GetCustomAttribute<ServiceAttribute>() != null;
-
-    private static List<ServiceInfo> GetServices()
-    {
-        return AppDomain.CurrentDomain.GetServices();
-    }
-
-    private static ServiceInfo GetService(Type type)
-    {
-        var serviceInfo = new ServiceInfo
-        {
-            Type = type,
-            Lifetime = type.GetCustomAttribute<ServiceAttribute>()!.Lifetime,
-            BaseTypes = new List<Type>()
-        };
-
-        type.GetInterfaces()
-            .ToList()
-            .ForEach(serviceInfo.BaseTypes.Add);
-
-        var baseType = type.BaseType;
-        if (baseType != typeof(object))
-            serviceInfo.BaseTypes.Add(baseType!);
-
-        serviceInfo.BaseTypes.Add(type);
-        return serviceInfo;
     }
 }
