8d761d5261f2b82175a0bdedf576f63247334d98
Javlon Fattoev <javlonfattoev@gmail.com>
2025-03-25 02:22:28 +0500

    support multiple .NET versions

--- JFA.DependencyInjection/Models/ServiceInfo.cs
+++ JFA.DependencyInjection/Models/ServiceInfo.cs
@@ -2,9 +2,9 @@
 
 public class ServiceInfo
 {
-    public Type Type { get; set; }
+    public Type Type { get; set; } = null!;
 
-    public List<Type> BaseTypes { get; set; }
+    public List<Type> BaseTypes { get; set; } = new List<Type>();
 
     public ELifetime Lifetime { get; set; }
 }
